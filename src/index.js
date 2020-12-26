ca381202a8b6676ffca0c89318af6f70e6125ab7
lovisasars <74593044+lovisasars@users.noreply.github.com>
2020-12-26 16:10:55 +0100

    added index.js file

---
+++ src/index.js
@@ -0,0 +1,58 @@
+function showPosition(position) {
+  console.log(position);
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "8eb322b04629a0b2fdac0ac79561148e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function showTemp(response) {
+  console.log(response);
+  let currentCity = response.data.name;
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${currentCity}`;
+  let temperature = Math.round(response.data.main.temp);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = temperature;
+}
+
+function getPosition(event) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let locationButton = document.querySelector("#location-button");
+locationButton.addEventListener("click", getPosition);
+
+function changeCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-value");
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${searchInput.value}`;
+  let apiUrlName = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=8eb322b04629a0b2fdac0ac79561148e&units=metric`;
+  axios.get(apiUrlName).then(changeTemp);
+}
+function changeTemp(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = temperature;
+}
+
+let form = document.querySelector("#search-city");
+form.addEventListener("submit", changeCity);
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let hour = now.getHours();
+let minute = now.getMinutes();
+let h3 = document.querySelector("h3");
+h3.innerHTML = `${day} ${hour}:${minute}`;
