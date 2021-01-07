259ffd50976e261aecff8585bd73de9537750718
lovisasars <74593044+lovisasars@users.noreply.github.com>
2021-01-07 15:58:30 +0100

    added fahrenheit calculation function
    made links from C to F to change unit of temperature data

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ function showTemp(response) {
   let currentCity = response.data.name;
   let h2 = document.querySelector("h2");
   h2.innerHTML = `${currentCity}`;
-  let temperature = Math.round(response.data.main.temp);
+  let celsiusTemperature = Math.round(response.data.main.temp);
   let tempNumber = document.querySelector("#temp-number");
   let weatherIcon = document.querySelector("#icon");
-  tempNumber.innerHTML = temperature;
+  tempNumber.innerHTML = celsiusTemperature;
   weatherIcon.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -27,21 +27,24 @@ function getPosition(event) {
 let locationButton = document.querySelector("#location-button");
 locationButton.addEventListener("click", getPosition);
 
-function changeCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#city-value");
+function changeCity(city) {
   let h2 = document.querySelector("h2");
-  h2.innerHTML = `${searchInput.value}`;
-  let apiUrlName = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=8eb322b04629a0b2fdac0ac79561148e&units=metric`;
+  h2.innerHTML = `${city}`;
+  let apiUrlName = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8eb322b04629a0b2fdac0ac79561148e&units=metric`;
   axios.get(apiUrlName).then(changeTemp);
+
+  apiUrlName = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8eb322b04629a0b2fdac0ac79561148e&units=metric`;
+  axios.get(apiUrlName).then(showIcon);
 }
 
-function changeTemp(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let tempNumber = document.querySelector("#temp-number");
-  let weatherIcon = document.querySelector("#icon");
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-value");
+  changeCity(searchInput.value);
+}
 
-  tempNumber.innerHTML = temperature;
+function showIcon(response) {
+  let weatherIcon = document.querySelector("#icon");
   weatherIcon.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -49,8 +52,34 @@ function changeTemp(response) {
   weatherIcon.setAttribute("alt", response.data.weather[0].description);
 }
 
+function changeTemp(response) {
+  celsiusTemperature = Math.round(response.data.main.temp);
+  let tempNumber = document.querySelector("#temp-number");
+  tempNumber.innerHTML = celsiusTemperature;
+}
+function showFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let tempNumber = document.querySelector("#temp-number");
+  tempNumber.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function showCelsius(event) {
+  event.preventDefault();
+  let tempNumber = document.querySelector("#temp-number");
+  tempNumber.innerHTML = celsiusTemperature;
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsius);
+
 let form = document.querySelector("#search-city");
-form.addEventListener("submit", changeCity);
+form.addEventListener("submit", searchCity);
+
+let celsiusTemperature = null;
 
 let now = new Date();
 let days = [
@@ -68,3 +97,5 @@ let hour = now.getHours();
 let minute = now.getMinutes();
 let h3 = document.querySelector("h3");
 h3.innerHTML = `${day} ${hour}:${minute}`;
+
+changeCity("New York");
